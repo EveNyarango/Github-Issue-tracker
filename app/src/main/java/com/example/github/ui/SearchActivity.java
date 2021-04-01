fd2c28a9a60d59931a195a4504fff3fca90ccbb4
EveNyarango <evenyarango@gmail.com>
2021-04-02 01:05:16 +0300

    add roboelectic and espresso dependencies

--- app/src/main/java/com/example/github/ui/SearchActivity.java
+++ app/src/main/java/com/example/github/ui/SearchActivity.java
@@ -39,6 +39,7 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 searchUsername();
+
             }
 
         });
